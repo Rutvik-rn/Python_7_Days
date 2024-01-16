07f814a3ccdccf40e190ac621802bd1fbd06c595
Rutvik N <62597322+Rutvik-rn@users.noreply.github.com>
2024-01-16 13:53:54 +0530

    Add files via upload

---
+++ Day-1_BandNameGenerator.py
@@ -0,0 +1,18 @@
+"""Build a Program asking the user for their city name and favourite noun from the list below or any other.
+List = [ 'Dragons', 'Sixers', 'Rangers', 'Rhythm', 'Vortex', 'Serenade', 'Galaxy', 'Echo']
+Combine the same to suggest a new Band Name.
+Learnings : Basic Input/Output, Variables, Strings, print function.
+"""
+
+print("Hello And " + "Welcome to the Band Name Generator 2024")
+
+C_name = input("Please Enter one name for the City where you belong to\n")
+
+print("'Dragons', 'Sixers', 'Rangers', 'Rhythm', 'Vortex', 'Serenade', 'Galaxy', 'Echo' ")
+
+N_name = input("Please Enter any noun from the List or Enter any other\n")
+
+print("Creating an Amazing Band Name...")
+
+print("Your Band Name Could Be:" + C_name + " " + N_name)
+
